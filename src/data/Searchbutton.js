cf78144fa638eb47d056c7a37a42736e9b80e355
vikas belida <VikasBelida99@gmail.com>
2020-08-29 16:30:03 +0530

    refactored searchPage by adding json configurations for buttons

---
+++ src/data/Searchbutton.js
@@ -0,0 +1,27 @@
+export const searchPageButtons = [
+  {
+    id: 1,
+    name: "Cancellation Flexibility",
+    variant: "outlined",
+  },
+  {
+    id: 2,
+    name: "Type of place",
+    variant: "outlined",
+  },
+  {
+    id: 3,
+    name: "Price",
+    variant: "outlined",
+  },
+  {
+    id: 4,
+    name: "Rooms and beds",
+    variant: "outlined",
+  },
+  {
+    id: 5,
+    name: "More filters",
+    variant: "outlined",
+  },
+];
